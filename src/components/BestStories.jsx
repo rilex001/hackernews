61d8d9fff5b176623162ddaa0c12d7c523f924c9
rilex001 <ristic1900@gmail.com>
2020-07-08 23:57:39 +0200

    responsive design

--- src/components/BestStories.jsx
+++ src/components/BestStories.jsx
@@ -6,7 +6,7 @@ function BestStories() {
     const [country, setCountry] = useState('us')
     const [category, setCategory] = useState('general')
     useEffect(() => {
-        fetch(`http://newsapi.org/v2/top-headlines?country=${country}&category=${category}&pageSize=100&apiKey=96856bfaa1f84df08dd045a49a76f737`)
+        fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&pageSize=100&apiKey=96856bfaa1f84df08dd045a49a76f737`)
         .then(res => res.json())
         .then(result => setStories(result.articles))
         .catch(err => {
